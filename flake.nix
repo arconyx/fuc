1f30ed09e67cbbf2c04769d15fcdabe6eba8ca0c
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-07-26 21:40:02 +1200

    Init with nix flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.gleam ];
+      };
+    };
+}
