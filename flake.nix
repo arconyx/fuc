c08675ef10764ac73c06431d016712ffffe8c202
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-09-12 15:25:33 +1200

    Remove outdated comment

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  # I'd prefer nixpkgs-unstable but rebar3 is currently broken there
-  # It's fixed, we're just waiting on hydra to catch up
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs =
