86f6d3a6d419b820dd558528a9deabf35d07fee7
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2026-09-18 13:43:13 +1200

    Revert "Let nix figure out if we're running tests"
    This reverts commit 3941db4b5f074be5e01c87f772a4e0be692efe95.
    
    I misread the docs. `doCheck` defaults false (why???) and is IGNORED when
    cross compiling

--- package.nix
+++ package.nix
@@ -130,6 +130,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   # This recompiles the entire thing to run the tests, making it *slow*
   # Still, some tests beats no tests
+  doCheck = true;
   checkPhase = ''
     runHook preCheck
     gleam test
